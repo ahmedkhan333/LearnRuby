cafa7782eaf649d717f0902044d161dd6bd09c15
ahmedkhan333 <bitf17a553@pucit.edu.pk>
2021-08-27 15:00:58 +0500

    added Some of the functionalities to my first learning ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  #GEt /about
+ 
+  get "about",to:"about#index"
+  
+ root to:"main#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
